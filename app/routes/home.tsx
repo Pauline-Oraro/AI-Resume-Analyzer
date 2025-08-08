bf36bb73156ecdde26a80e449599fb6256904b8d
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-08 15:51:20 +0300

    Implementing the resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "AI RESUME ANALYZER" },
-    { name: "description", content: "AI RESUME ANALYZER" },
+    { name: "description", content: "AI Resume Analyzer - Instantly evaluate resumes with AI-powered insights" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings.</h1>
+        <h2>Review Your Submissions and Check AI-powered feedback.</h2>
+      </div>
+
+      {/*if resumes are greater than 0 display the div */}
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+     <ResumeCard key={resume.id} resume={resume}/>
+    ))}
+      </div>
+    )}
+
+
+    </section>
+
+  </main>;
 }

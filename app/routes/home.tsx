1d5475850e018a3f3537fffa1deef19794ef749d
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-09 15:23:17 +0300

    added authentication and updated the meta title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,15 +2,26 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "AI RESUME ANALYZER" },
+    { title: "RESUME ANALYZER" },
     { name: "description", content: "AI Resume Analyzer - Instantly evaluate resumes with AI-powered insights" },
   ];
 }
 
 export default function Home() {
+
+    const {auth} = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+  
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
 

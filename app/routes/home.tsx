8e27912aefb7968182b1dd62949c079117729395
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-12 12:32:33 +0300

    updated the home section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "~/constants";
-import ResumeCard from "~/components/ResumeCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter"
+import { Link } from "react-router"
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,31 +16,30 @@ export default function Home() {
 
     const {auth} = usePuterStore();
     const navigate = useNavigate();
+    
 
     useEffect(() => {
     if(!auth.isAuthenticated) navigate('/auth?next=/');
   }, [auth.isAuthenticated])
+
+  
   
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
 
     <section className="main-section">
       <div className="page-heading py-16">
-        <h1>Track Your Applications & Resume Ratings.</h1>
-        <h2>Review Your Submissions and Check AI-powered feedback.</h2>
+        <h1>Track Your Applications & Resume Ratings</h1>
+        
+            <h2>Upload your first resume to get feedback.</h2>
+       
       </div>
-
-      {/*if resumes are greater than 0 display the div */}
-
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map((resume) => (
-     <ResumeCard key={resume.id} resume={resume}/>
-    ))}
-      </div>
-    )}
-
-
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+    
     </section>
 
   </main>;
